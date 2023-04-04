420a39fae4bf40e044c32e3aa64bdfcb67258676
RickyAdriell <53133228+codecplyre@users.noreply.github.com>
2023-04-04 16:29:24 +0100

    separate database to its own folder

--- movie-service/db/migrations/20230403224913_create_movies_table.up.sql
+++
@@ -1,7 +0,0 @@
--- Active: 1680618150350@@127.0.0.1@3306@movie_service
-CREATE TABLE IF NOT EXISTS movies (
-    id SERIAL PRIMARY KEY,
-    title VARCHAR(255) NOT NULL UNIQUE,
-    release_date DATE NOT NULL,
-    director VARCHAR(255) NOT NULL
-);
